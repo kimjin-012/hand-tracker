0b5ff433dbbe44b2bf2641d9789c288ac582b1af
kimjin-012 <77659317+kimjin-012@users.noreply.github.com>
2021-06-14 20:47:44 -0700

    Update HandTrackMin.py

--- HandTrackMin.py
+++ HandTrackMin.py
@@ -4,14 +4,13 @@ import time
 
 cap = cv2.VideoCapture(0)
 
-# mpHands = mp.solutions.hands
-# hands = mpHands.Hands()
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
 
 while True:
     success, img = cap.read()
-    # imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    # results = hands.process(imgRGB)
-
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
