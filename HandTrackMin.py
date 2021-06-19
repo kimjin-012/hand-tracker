d3401271e20e8213244ce25ee26a27ff05257479
kimjin-012 <77659317+kimjin-012@users.noreply.github.com>
2021-06-18 21:16:24 -0700

    Hand tracking basic built

--- HandTrackMin.py
+++ HandTrackMin.py
@@ -2,11 +2,15 @@ import cv2
 import mediapipe as mp
 import time
 
+# Using the pc default webcam
 cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
+
+prevTime = 0
+currTime = 0
 
 while True:
     success, img = cap.read()
@@ -16,7 +20,21 @@ while True:
 
     if results.multi_hand_landmarks:
        for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+           for id, lm in enumerate(handLms.landmark):
+               h, w, c = img.shape
+               cx, cy = int(lm.x * w), int(lm.y * h)
+               # getting the desired point and mark it
+               if id == 4:
+                   cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+
+               mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+    currTime = time.time()
+    fps = 1/(currTime - prevTime)
+    prevTime = currTime
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
 
     cv2.imshow("Image", img)
+    # 1 means 1 millisecond
     cv2.waitKey(1)
